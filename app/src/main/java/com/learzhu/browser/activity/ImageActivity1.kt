3777baf433b6bf902652b7a8e58e190710e9ebd2
Learzhu <694125196@qq.com>
2017-08-03 00:42:57 +0800

    build修改支持kotlin 新增一个ImageActivity1为kotlin的

---
+++ app/src/main/java/com/learzhu/browser/activity/ImageActivity1.kt
@@ -0,0 +1,14 @@
+package com.learzhu.browser.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+import com.learzhu.browser.R
+
+class ImageActivity1 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image)
+    }
+}
