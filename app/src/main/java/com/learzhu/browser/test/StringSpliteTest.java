41cb86c0766812b7bc6a94e0a190bfb743fe9cd8
Learzhu <694125196@qq.com>
2018-12-27 13:30:49 +0800

    1.String的测试

--- app/src/main/java/com/learzhu/browser/test/StringSpliteTest.java
+++ app/src/main/java/com/learzhu/browser/test/StringSpliteTest.java
@@ -1,8 +1,8 @@
 package com.learzhu.browser.test;
 
-import com.learzhu.browser.utils.StringUtils;
-
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * ${className}.java是极搜浏览器的$DES$类。
@@ -35,7 +35,51 @@ public class StringSpliteTest {
 //        }
 
 //        String dateWithoutSec = StringUtils.getDateWithoutSec("2018-05-06 12:22:33");
-        String dateWithoutSec = StringUtils.getDateWithoutSec(" ");
-        System.out.println(dateWithoutSec);
+//        String dateWithoutSec = StringUtils.getDateWithoutSec(" ");
+//        System.out.println(dateWithoutSec);
+        System.out.println(changeRecent("E"));
+//        System.out.println(getRecentVisitCitiesStringList());
     }
+
+    public static String changeRecent(String city) {
+        String recentCities = "A,B,C,D,E,F,";
+//        String recentCities = "";
+//        String recentCities = "E";
+        if (recentCities.isEmpty()) {
+            recentCities = city;
+        } else {
+            String[] strings = recentCities.split(",");
+            boolean isExisted = false;
+            //遍历保存新的访问地址 更新历史和位置排序
+            for (int i = 0; i < strings.length; i++) {
+                if (city.equals(strings[i])) {
+                    //如果存在就更换顺序
+                    isExisted = true;
+                    StringBuilder citiesSb = new StringBuilder();
+                    citiesSb.append(city).append(",");
+                    for (int j = 0; j < strings.length && j != i; j++) {
+                        citiesSb.append(strings[j]).append(",");
+                    }
+                    recentCities = citiesSb.toString();
+                    break;
+                }
+            }
+            if (!isExisted) {
+                //如果之前不存在的就添加到开头
+                recentCities = city + "," + recentCities;
+            }
+        }
+        return recentCities;
+    }
+
+    public static List<String> getRecentVisitCitiesStringList() {
+        String recentCities = "";
+        String[] split = recentCities.split(",");
+        List<String> cities = new ArrayList<>();
+        for (String city : split) {
+            cities.add(city);
+        }
+        return cities;
+    }
+
 }
