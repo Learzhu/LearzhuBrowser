3abe8982c54683ad4890600ffb7db5bd475c1dae
Learzhu <694125196@qq.com>
2020-07-10 18:03:59 +0800

    1.新增 constraintLayout的练习

---
+++ app/src/main/java/com/learzhu/browser/activity/Main4Activity.java
@@ -0,0 +1,15 @@
+package com.learzhu.browser.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.learzhu.browser.R;
+
+public class Main4Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+    }
+}
